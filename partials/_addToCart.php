4e6912b6c85cbfafad923e57a246556f28e787ec
Md. Irtiza Hossain <mdirtiza.hossain1999@gmail.com>
2022-10-14 16:49:27 +0600

    Done

--- partials/_addToCart.php
+++ partials/_addToCart.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include 'partials/_dbconnect.php';
+include '_dbconnect.php';
 
 if(!(isset($_SESSION["cart"]))){
     $_SESSION["cart"];
@@ -8,14 +8,24 @@ if(!(isset($_SESSION["cart"]))){
  if(isset($_POST["id"])){
         $id = $_POST["id"];
         $quan = $_POST["quan"];
-        if($quan>0 && filter_var($quan,FILTER_VALIDATE_INT)){
+        $sql = "SELECT * FROM `products` WHERE `product_id` = $id";
+        $result = mysqli_query($conn, $sql);
+        $stock = 0;
+        while($row = mysqli_fetch_assoc($result)){
+            $stock = $row['product_stock'];
+        }
+        if($quan>0 && $quan <= $stock && filter_var($quan,FILTER_VALIDATE_INT)){
             if(isset($_SESSION['cart'][$id])){
                 $_SESSION['cart'][$id] += $quan;
             }else{
                 $_SESSION['cart'][$id] = $quan;
             }
+            // $sql = "Update `products` SET `product_stock` = `product_stock` - $quan WHERE `product_id` = $id";
+            // $result = mysqli_query($conn, $sql);
+            
         }else{
             echo '<h1>Invalid quantity</h1>';
+            exit();
         }
         
     }
--- shoppingCart.php
+++ shoppingCart.php
@@ -20,125 +20,135 @@
     ?>
     <div class="container">
         <?php
-
-                                    if(!empty($_SESSION["cart"]))
-                                    {
-                                    include 'partials/_dbconnect.php';
-                                    $total = 0;
-                                    $arr = $_SESSION["cart"];
-                                    $keys = array_keys($arr);
-                                    $bill = 0;
-                                    foreach($arr as $i => $j){
-                                        $sql = "SELECT * FROM `products` WHERE `product_id` = $i";
-                                        $result = mysqli_query($conn, $sql);
-                                        while($row = mysqli_fetch_assoc($result)){
-                                            $price = $row['product_price'];
-                                            $name = $row['product_name'];
-                                            $image = $row['product_image'];
-                                            $stock = $row['product_stock'];
-                                            $total = $row['product_price'] * $j;
-                                            $bill += $total;
-                                        }
-                                    
-                                    echo'
-                                    <div class="row">
-                                    <div class="col-sm-12 col-md-10 col-md-offset-1">
-                                        <table class="table table-hover">
-                                            <thead>
-                                                <h1 style="text-align: center; padding-top: 50px; padding-bottom: 20px;">You Products</h1>
-                                                <tr>
-                                                    <th>
-                                                    </th>
-                                                    <th>Quantity</th>
-                                                    <th class="text-center">Price</th>
-                                                    <th class="text-center">Total</th>
-                                                    <th> </th>
-                                                </tr>
-                                            </thead>
-                                    <tbody>
-                                    <tr>
-                                    
-                                    <td class="col-sm-8 col-md-4">
-                                    <div class="media">
-                                    
-                                    <a class="thumbnail pull-lef" href="#"> <img class="media-object"
-                                            src="'.$image.'"
-                                            style="width: 72px; height: 72px;"> </a>
-                                    <div class="media-body">
-                                        <h4 class="media-heading"><a href="/store/productPage.php?productId='.$i.'">'.$name.'</a></h4>
-                                        <span style="padding-top: 5px;">Status: </span><span
-                                            class="text-success"><strong>In
-                                                Stock</strong></span>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="col-sm-1 col-md-1" style="text-align: center">
-                                <input type="email" class="form-control" id="exampleInputEmail1" value="'.$j.'">
-                            </td>
-                            <td class="col-sm-1 col-md-1 text-center"><strong>$'.$price.'</strong></td>
-                            <td class="col-sm-1 col-md-1 text-center"><strong>$'.$total.'</strong></td>
-                            <td class="col-sm-1 col-md-1">
-                                <button type="button" class="btn btn-danger">
-                                    <span class="glyphicon glyphicon-remove"></span> Remove</button>
-                            </td>
-                        </tr>';
- }
-                        echo'<tr>
-                            <td>   </td>
-                            <td>   </td>
-                            <td>   </td>
-                            <td>
-                                <h5>Subtotal</h5>
-                            </td>
-                            <td class="text-right">
-                                <h5><strong>$'.$bill.'</strong></h5>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>   </td>
-                            <td>   </td>
-                            <td>   </td>
-                            <td>
-                                <h5>Estimated shipping</h5>
-                            </td>
-                            <td class="text-right">
-                                <h5><strong>$6.94</strong></h5>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>   </td>
-                            <td>   </td>
-                            <td>   </td>
-                            <td>
-                                <h3>Total</h3>
-                            </td>
-                            <td class="text-right">
-                                <h3><strong>$'.$bill.'</strong></h3>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>   </td>
-                            <td>   </td>
-                            <td>   </td>
-                            <td>
-                                <a href="/store/index.php">
-                                <button type="button" class="btn btn-primary">
-                                    Continue Shopping
-                                </button>
-                                </a>
-                            </td>
-                            <td>
-                            <a href="/store/checkout.php">
-                                <button type="button" class="btn btn-success">
-                                    Checkout <span class="glyphicon glyphicon-play"></span>
-                                </button>
-                            </a>
-                            </td>
-                        </tr>';}
-                        else{
-                            echo '<h1 style="text-align: center; padding-top: 50px; padding-bottom: 20px;">Your Cart is Empty</h1>';
-                        }
-                        ?>
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                    $removeId = $_POST["removeId"];
+                    $item = $_POST["removeItem"];
+                    if (($key = array_search($removeId, $_SESSION["cart"])) !== false) {
+                        unset($_SESSION["cart"][$removeId]);
+                }}
+                if(!empty($_SESSION["cart"])){
+                echo '<h1 style= "padding-top: 50px; padding-bottom: 20px;">You Products</h1>
+                        <div class="row">
+                <div class="col-sm-12 col-md-10 col-md-offset-1">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>
+                                </th>
+                                <th>Quantity</th>
+                                <th class="text-center">Price</th>
+                                <th class="text-center">Total</th>
+                                <th> </th>
+                            </tr>
+                        </thead>
+                ';
+                include 'partials/_dbconnect.php';
+                $total = 0;
+                $arr = $_SESSION["cart"];
+                $keys = array_keys($arr);
+                $bill = 0;
+                foreach($arr as $i => $j){
+                    $sql = "SELECT * FROM `products` WHERE `product_id` = $i";
+                    $result = mysqli_query($conn, $sql);
+                    while($row = mysqli_fetch_assoc($result)){
+                        $price = $row['product_price'];
+                        $name = $row['product_name'];
+                        $image = $row['product_image'];
+                        $stock = $row['product_stock'];
+                        $total = $row['product_price'] * $j;
+                        $bill += $total;
+                    }
+                
+                echo'
+                
+                <tbody>
+                <tr>
+                
+                <td class="col-sm-8 col-md-4">
+                <div class="media">
+                
+                <a class="thumbnail pull-lef" href="#"> <img class="media-object"
+                        src="'.$image.'"
+                        style="width: 72px; height: 72px;"> </a>
+                <div class="media-body">
+                    <h4 class="media-heading"><a href="/store/productPage.php?productId='.$i.'">'.$name.'</a></h4>
+                    <span style="padding-top: 5px;">Status: </span><span
+                        class="text-success"><strong>In
+                            Stock</strong></span>
+                </div>
+            </div>
+        </td>
+        <td class="col-sm-1 col-md-1" style="text-align: center">
+            <input type="email" class="form-control" id="exampleInputEmail1" value="'.$j.'">
+        </td>
+        <td class="col-sm-1 col-md-1 text-center"><strong>$'.$price.'</strong></td>
+        <td class="col-sm-1 col-md-1 text-center"><strong>$'.$total.'</strong></td>
+        <td class="col-sm-1 col-md-1">';
+        echo'
+            <form action='.$_SERVER['PHP_SELF'].' method="post">
+            <button name= "removeItem" type="button" class="btn btn-danger">
+            <input type="hidden" name="removeId" value="'.$i.'">
+                <span class="glyphicon glyphicon-remove"></span> Remove</button>
+            </form>
+        </td>
+    </tr>';
+    }
+    echo'<tr>
+        <td>   </td>
+        <td>   </td>
+        <td>   </td>
+        <td>
+            <h5>Subtotal</h5>
+        </td>
+        <td class="text-right">
+            <h5><strong>$'.$bill.'</strong></h5>
+        </td>
+    </tr>
+    <tr>
+        <td>   </td>
+        <td>   </td>
+        <td>   </td>
+        <td>
+            <h5>Estimated shipping</h5>
+        </td>
+        <td class="text-right">
+            <h5><strong>$6.94</strong></h5>
+        </td>
+    </tr>
+    <tr>
+        <td>   </td>
+        <td>   </td>
+        <td>   </td>
+        <td>
+            <h3>Total</h3>
+        </td>
+        <td class="text-right">
+            <h3><strong>$'.$bill.'</strong></h3>
+        </td>
+    </tr>
+    <tr>
+        <td>   </td>
+        <td>   </td>
+        <td>   </td>
+        <td>
+            <a href="/store/index.php">
+            <button type="button" class="btn btn-primary">
+                Continue Shopping
+            </button>
+            </a>
+        </td>
+        <td>
+        <a href="/store/checkout.php">
+            <button type="button" class="btn btn-success">
+                Checkout <span class="glyphicon glyphicon-play"></span>
+            </button>
+        </a>
+        </td>
+    </tr>';}
+    else{
+        echo '<h1 style="text-align: center; padding-top: 50px; padding-bottom: 20px;">Your Cart is Empty</h1>';
+    }
+    ?>
         </tbody>
         </table>
     </div>
