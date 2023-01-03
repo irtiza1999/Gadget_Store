031e60926c0f23d780f62166f4647ec1b09e61e8
Md. Irtiza Hossain <73892941+irtiza1999@users.noreply.github.com>
2023-01-03 22:14:46 +0600

    Update config.php

--- partials/config.php
+++ partials/config.php
@@ -2,8 +2,8 @@
     require_once "../stripe-php-master/init.php";
 
     $stripeDetails = array(
-        "secretKey" => "sk_test_51HubhJHonQARCwke5cz63EUvujll52lLSguvPnc7YHumC1qj32c58WRfOtpROnIGvkxatoclqdzKwu3K08C3ZeXi00xg0Pntiw",
-        "publishableKey" => "pk_test_51HubhJHonQARCwkeqhDsoRgvql1fz9wDOl3tY2LwQ67mYp06UBDvNAJ45pS3Zffa2rIJMt22ATNLieFUq8OzDDfS00IB1GOyfL"
+        "secretKey" => "SECRETKEY",
+        "publishableKey" => "PublishableKey"
     );
     \Stripe\Stripe::setApiKey($stripeDetails["secretKey"]);
 ?>
